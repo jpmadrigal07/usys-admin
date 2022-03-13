1711304f952ca476303441b000247aefd29329bb
Rene Serwyn Gimutao <serwyngimutao@gmail.com>
2022-03-13 17:32:45 +0800

    0027-Create Settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <div className="w-full pl-16 py-10"> Module Settings</div>
+      <div className="pl-72"> Settings </div>
+    </div>
+  );
+};
+
+export default Settings;
