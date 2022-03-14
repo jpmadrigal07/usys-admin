f9369da3152d8511d3de64a30ed766093980ebed
Rene Serwyn Gimutao <serwyngimutao@gmail.com>
2022-03-14 17:40:58 +0800

    Changes for 0.0.7

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -13,30 +13,42 @@ const Settings = () => {
       </div>
       <div className="pl-72 text-ash-gray"> Settings </div>
       <div className="grid grid-cols-6 mx-72 py-8 justify-items-center ;\">
-        <div className="text-center grid justify-items-center">
-          {<Icon icon="bi:person-plus" fontSize={45} color="#006fc9" />}
-          <span className="text-ash-gray pt-3">Student Type</span>
-        </div>
-        <div className="text-center grid justify-items-center">
-          {<Icon icon="bi:person-video3" fontSize={45} color="#006fc9" />}
-          <span className="text-ash-gray pt-3">Student Level</span>
-        </div>
-        <div className="text-center grid justify-items-center">
-          {<Icon icon="bi:calendar-week" fontSize={45} color="#006fc9" />}
-          <span className="text-ash-gray pt-3">Semester</span>
-        </div>
-        <div className="text-center grid justify-items-center">
-          {<Icon icon="bi:book-half" fontSize={45} color="#006fc9" />}
-          <span className="text-ash-gray pt-3">Subject</span>
-        </div>
-        <div className="text-center grid justify-items-center">
-          {<Icon icon="bi:journal-bookmark" fontSize={45} color="#006fc9" />}
-          <span className="text-ash-gray pt-3">Subject Prerequisite</span>
-        </div>
-        <div className="text-center grid justify-items-center">
-          {<Icon icon="bi:card-list" fontSize={45} color="#006fc9" />}
-          <span className="text-ash-gray pt-3">Curriculum</span>
-        </div>
+        <a href="#" className={` hover:bg-gray px-4 h-full p-4 `}>
+          <div className="text-center grid justify-items-center">
+            {<Icon icon="bi:person-plus" fontSize={45} color="#006fc9" />}
+            <span className="text-ash-gray pt-3">Student Type</span>
+          </div>
+        </a>
+        <a href="#" className={` hover:bg-gray px-4 h-full p-4 `}>
+          <div className="text-center grid justify-items-center">
+            {<Icon icon="bi:person-video3" fontSize={45} color="#006fc9" />}
+            <span className="text-ash-gray pt-3">Student Level</span>
+          </div>
+        </a>
+        <a href="#" className={` hover:bg-gray px-4 h-full p-4 `}>
+          <div className="text-center grid justify-items-center">
+            {<Icon icon="bi:calendar-week" fontSize={45} color="#006fc9" />}
+            <span className="text-ash-gray pt-3">Semester</span>
+          </div>
+        </a>
+        <a href="#" className={` hover:bg-gray px-4 h-full p-4 `}>
+          <div className="text-center grid justify-items-center">
+            {<Icon icon="bi:book-half" fontSize={45} color="#006fc9" />}
+            <span className="text-ash-gray pt-3">Subject</span>
+          </div>
+        </a>
+        <a href="#" className={` hover:bg-gray px-4 h-full p-4 `}>
+          <div className="text-center grid justify-items-center">
+            {<Icon icon="bi:journal-bookmark" fontSize={45} color="#006fc9" />}
+            <span className="text-ash-gray pt-3">Subject Prerequisite</span>
+          </div>
+        </a>
+        <a href="#" className={` hover:bg-gray px-4 h-full p-4`}>
+          <div className="text-center grid justify-items-center">
+            {<Icon icon="bi:card-list" fontSize={45} color="#006fc9" />}
+            <span className="text-ash-gray pt-3">Curriculum</span>
+          </div>
+        </a>
       </div>
     </div>
   );
