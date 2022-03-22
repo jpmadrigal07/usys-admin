c26446c38823929225fa84cf42ca6db796d6323a
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-03-22 23:04:59 +0800

    Added changes for 0.0.16

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = () => {
   return (
@@ -10,7 +8,6 @@ const MainLayout = () => {
       <NavBar />
       <div className="p-3">
         <Outlet />
-        <ToastContainer />
       </div>
     </>
   );
