53a75cb232a935f7f293003eedae0649814982ef
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-03-28 08:01:12 +0800

    Added changes for 0.0.13

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,73 +1,81 @@
-import React from "react";
 import CardMenu from "../components/CardMenu";
 import I_Usys_Logo from "../assets/usys_logo.png";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { setAuthenticatedUser } from "../actions/authenticatedUserActions";
 import Cookies from "js-cookie";
 
-const navigate = useNavigate();
-const sessionToken = Cookies.get("sessionToken");
-
-const _removeSessionToken = () => {
-  if (sessionToken) {
-    Cookies.remove("sessionToken");
-    setAuthenticatedUser({});
-    console.log("removeSession");
-    navigate("/");
-  }
-};
-const cards = [
-  {
-    id: "4234s934",
-    title: "Logout",
-    img: "./static/logout_logo.png",
-    link: _removeSessionToken(),
-  },
-  {
-    id: "4234s24",
-    title: "Accounting",
-    img: "./static/accounting_logo.png",
-    link: "/module/accounting",
-  },
-  {
-    id: "42346724",
-    title: "Admission",
-    img: "./static/admission_logo.png",
-    link: "/module/admission",
-  },
-  {
-    id: "4234a2n4",
-    title: "Campuses",
-    img: "./static/campuses_logo.png",
-    link: "/module/campuses",
-  },
-  {
-    id: "423d424y",
-    title: "Cashier",
-    img: "./static/cashier_logo.png",
-    link: "/module/cashier",
-  },
-  {
-    id: "42345624",
-    title: "Entrance Exam",
-    img: "./static/entrance_exam_logo.png",
-    link: "/module/entrance-exam",
-  },
-  {
-    id: "4234s274",
-    title: "Register",
-    img: "./static/registrar_logo.png",
-    link: "/module/registrar",
-  },
-  {
-    id: "4234324",
-    title: "Settings",
-    img: "./static/settings_logo.png ",
-    link: "/module/settings",
-  },
-];
-
 const MainMenu = () => {
+  const navigate = useNavigate();
+  const sessionToken = Cookies.get("sessionToken");
+
+  const _removeSessionToken = () => {
+    if (sessionToken) {
+      Cookies.remove("sessionToken");
+      setAuthenticatedUser({});
+      console.log("removeSession");
+      window.location.href = "/";
+    }
+  };
+
+  const cards = [
+    {
+      id: "4234s934",
+      title: "Logout",
+      img: "./static/logout_logo.png",
+      isLogout: true,
+      link: "",
+    },
+    {
+      id: "4234s24",
+      title: "Accounting",
+      img: "./static/accounting_logo.png",
+      link: "/module/accounting",
+      isLogout: null,
+    },
+    {
+      id: "42346724",
+      title: "Admission",
+      img: "./static/admission_logo.png",
+      link: "/module/admission",
+      isLogout: null,
+    },
+    {
+      id: "4234a2n4",
+      title: "Campuses",
+      img: "./static/campuses_logo.png",
+      link: "/module/campuses",
+      isLogout: null,
+    },
+    {
+      id: "423d424y",
+      title: "Cashier",
+      img: "./static/cashier_logo.png",
+      link: "/module/cashier",
+      isLogout: null,
+    },
+    {
+      id: "42345624",
+      title: "Entrance Exam",
+      img: "./static/entrance_exam_logo.png",
+      link: "/module/entrance-exam",
+      isLogout: null,
+    },
+    {
+      id: "4234s274",
+      title: "Register",
+      img: "./static/registrar_logo.png",
+      link: "/module/registrar",
+      isLogout: null,
+    },
+    {
+      id: "4234324",
+      title: "Settings",
+      img: "./static/settings_logo.png ",
+      link: "/module/settings",
+      isLogout: null,
+    },
+  ];
+
   return (
     <div className="h-auto py-5 lg:h-screen grid place-content-center">
       <div>
@@ -77,9 +85,15 @@ const MainMenu = () => {
         {cards.map((c) => {
           return (
             <div key={c.id} className="w-fit h-fit">
-              <Link to={`${c.link}`}>
+              <span
+                onClick={() =>
+                  c.isLogout
+                    ? _removeSessionToken()
+                    : navigate(c.link ? c.link : "/")
+                }
+              >
                 <CardMenu title={c.title} img={c.img} />
-              </Link>
+              </span>
             </div>
           );
         })}
